19263913c6c21b27c1d794d07c4a50b93d37ff16
Alecu Ștefan-Iulian <uneven-shiver@protonmail.com>
2023-03-31 00:59:09 +0300

    Nu ma mai ating vreodata de codul asta

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # a-din-i
 Convertor între scrierea cu î și scrierea cu â
+
+De ce l-am făcut? Pentru că de multe ori trebuie să convertesc între scrierea cu î din i pe care-o folosesc și scrierea cu â din a. Prin urmare, am făcut un mic script care să-mi ușureze munca. Codul e quick and dirty, am pus exact 0 efort în a-l face mai elegant, a se folosi ca atare.
+
+ATENȚIE: acest script modifică direct fișierul!
+```
+usage: convertor.py [-h] [-a] [-i] fisier
+
+Convertor â <-> î
+
+positional arguments:
+  fisier         Fișierul care va fi convertit
+
+options:
+  -h, --help     show this help message and exit
+  -a, --a-din-i  Convertește din â în î (default: False)
+  -i, --i-din-a  Convertește din î în â (default: False)
+```
--- convertor.py
+++ convertor.py
@@ -16,6 +16,7 @@
 import argparse
 import sys
 import fileinput
+from contextlib import redirect_stdout
 import re
 
 # https://wiki.ceata.org/index.php/Scrierea_cuvintelor_cu_%C3%AE_%C8%99i_%C3%A2_%C3%AEn_rom%C3%A2n%C4%83
@@ -35,17 +36,20 @@ def converteste_i_in_a(cuvinte: list[str]) -> list[str]:
     for i, _ in enumerate(cuvinte):
         cuvinte[i] = cuvinte[i].replace("î", "â")
 
-        if any(cuvinte[i].startswith(item) for item in lista_prefixe):
-            prefix = lista_prefixe[list(
-                map(lambda w: cuvinte[i].startswith(w), lista_prefixe)).index(True)]
-            # cuvinte[i] = cuvinte[i].replace("â", "î", len(prefix))
-            cuvinte[i] = cuvinte[i][:len(
-                prefix)] + "î" + cuvinte[i][len(prefix)+1:].replace("î", "â")
+        masca = list(map(lambda w: cuvinte[i].startswith(
+            w), lista_prefixe))
+
+        if any(masca):
+            prefix = lista_prefixe[masca.index(True)]
+            print(prefix, masca.index(True))
+            if cuvinte[i][len(prefix)] == "â":
+                cuvinte[i] = cuvinte[i][: len(
+                    prefix)] + "î" + cuvinte[i][len(prefix)+1:].replace("î", "â")
 
         if cuvinte[i].startswith("â"):
             cuvinte[i] = cuvinte[i].replace("â", "î", 1)
         if cuvinte[i].endswith("â"):
-            cuvinte[i] = cuvinte[i][::-1].replace("â", "î", 1)[::-1]
+            cuvinte[i] = cuvinte[i][:: -1].replace("â", "î", 1)[:: -1]
 
     return cuvinte
 
@@ -61,14 +65,13 @@ def main() -> None:
     args = parser.parse_args()
 
     try:
-        with fileinput.input(args.fisier, inplace=True) as fh:
-            for linie in fh:
-                cuvinte = re.split(r'(\S+)', linie)
-                if args.a_din_i:
-                    linie_noua: str = "".join(converteste_a_in_i(cuvinte))
-                elif args.i_din_a:
-                    linie_noua: str = "".join(converteste_i_in_a(cuvinte))
-                print(linie_noua, end='')
+        for linie in fileinput.input(args.fisier, inplace=True):
+            cuvinte = re.split(r'(\S+)', linie)
+            if args.a_din_i:
+                linie_noua: str = "".join(converteste_a_in_i(cuvinte))
+            elif args.i_din_a:
+                linie_noua: str = "".join(converteste_i_in_a(cuvinte))
+            print(linie_noua, end='')
 
     except FileNotFoundError:
         print(f"Fisierul {args.fisier} nu exista!", file=sys.stderr)
